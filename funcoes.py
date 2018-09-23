456d79e63d38f42b53dbd38b536cdc8c14f4a34e
Pedro <pedro@new-host-41.home>
2018-09-23 13:34:45 -0300

    adicionada a função de tratamento

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,23 @@ def geraAleatorios():
 
   return listaAlfa
 
+def filtroLinha(linhaAtual):
+  if (len(linhaAtual) == 10):
+    return linhaAtual
+  elif(len(linhaAtual) == 11) or (len(linhaAtual) == 12):
+    linhaNova = []
+    coluna = 0
+    while coluna < len(linhaAtual):
+      if ((linhaAtual[coluna] == 'quite') or (linhaAtual[coluna] =='rich') or (linhaAtual[coluna] == 'domestic') or (linhaAtual[coluna] == 'appliances')):
+        
+        if((linhaAtual[coluna] == 'quite') and (linhaAtual[coluna+1] == 'rich')):
+          linhaNova.append('quiteRich')
+
+        if((linhaAtual[coluna] == 'domestic') and (linhaAtual[coluna+1] == 'appliances')):
+          linhaNova.append('domesticAppliances')
+      else:
+        linhaNova.append(linhaAtual[coluna])
+
+      coluna += 1
+
+    return linhaNova
--- projetoCredito.py
+++ projetoCredito.py
@@ -16,11 +16,15 @@ cliente = {
 
 ref_arquivo = open("german_credit.txt", "r")
 linhas = (ref_arquivo.readline()).split('\t')
-
+count = 0
 for linha in ref_arquivo:
   linhaAtual = linha.split()
+  linhaNova = []
   #Tamanho menos um são as linhas com parametros corretos pelo fato de existir um parametro excedente que é real_Aprovação
   #9 - com todos os atributos certos
-  if(len(linhaAtual)-1) == 10:
-    print(linhaAtual[4] + " " + linhaAtual[5])
-  
+  #
+  linhaAtual = funcoes.filtroLinha(linhaAtual)
+  if(len(linhaAtual) == 9):
+    count += 1
+    print(linhaAtual)
+    print(count)
