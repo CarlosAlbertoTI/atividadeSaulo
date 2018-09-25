003d153e7b3579f33a803fd382d784ad2e109ed4
CarlosAlbertoTI <carlosjunior.alberto4@gmail.com>
2018-09-25 16:34:22 -0300

    Default Changelist

--- funcoes.py
+++ funcoes.py
@@ -14,8 +14,15 @@ def calibrar(lista,epoca,contador):
   lista_Alfa = geraAleatorios()
   if contador == 0 and epoca == 0:
       #agora eu tenho que chamar os valores de lista_random e lista para junta-los numa variavel IA
-      IA = (lista_Alfa[0]*lista["idade"])+(lista_Alfa[1]*lista["sexo"])+(lista_Alfa[2]*lista["emprego"])+(lista_Alfa[3]*lista["habitacao"])+(lista_Alfa[4]*lista["poucanca"])+(lista_Alfa[5]*lista["corrente"])+(lista_Alfa[6]*lista["credito"])+(lista_Alfa[7]*lista["duracao"])+(lista_Alfa[8]*lista["proposito"])
-      print(IA)
+      print('lista: ',int(lista["idade"]),' : ',lista_Alfa[0])
+      IA = (lista_Alfa[0]*lista["idade"])#+(lista_Alfa[1]*lista["sexo"])+(lista_Alfa[2]*lista["emprego"])+(lista_Alfa[3]*lista["habitacao"])+(lista_Alfa[4]*lista["poucanca"])+(lista_Alfa[5]*lista["corrente"])+(lista_Alfa[6]*lista["credito"])+(lista_Alfa[7]*lista["duracao"])+(lista_Alfa[8]*lista["proposito"])
+      maior_aleatorio = max(lista_Alfa)
+      return IA,maior_aleatorio
+      #print(IA)
   else:
-      IA = (lista_Alfa[0]*lista["idade"]) + (lista_Alfa[1]*lista["sexo"]) + (lista_Alfa[2] * lista["emprego"]) + (lista_Alfa[3] * lista["habitacao"]) + (lista_Alfa[4] * lista["poucanca"]) + (lista_Alfa[5] * lista["corrente"]) + (lista_Alfa[6] * lista["credito"]) + (lista_Alfa[7] * lista["duracao"]) + (lista_Alfa[8] * lista["proposito"])
-      print(IA)
+      #print('lista: ', int(lista["idade"]), ' : ', lista_Alfa[0])
+      #print('soma: ', int(lista["sexo"])+1,' : ',lista_Alfa[0])
+      IA = (lista_Alfa[0] * int(lista["idade"])) + (lista_Alfa[1] * int(lista["sexo"])) + (lista_Alfa[2] * int(lista["emprego"])) + (lista_Alfa[3] * int(lista["habitacao"])) + (lista_Alfa[4] * lista["poucanca"]) + (lista_Alfa[5] * lista["corrente"]) + (lista_Alfa[6] * lista["credito"]) + (lista_Alfa[7] * lista["duracao"]) + (lista_Alfa[8] * lista["proposito"])
+      maior_aleatorio = max(lista_Alfa)
+      return IA,maior_aleatorio
+      #print(IA)
--- projetoCredito.py
+++ projetoCredito.py
@@ -8,4 +8,5 @@ for epoca in range(50):
   contador = 0
   for cliente in lista_clientes:
     clientes = calibrar(cliente,epoca,contador)
+
     contador+=1
